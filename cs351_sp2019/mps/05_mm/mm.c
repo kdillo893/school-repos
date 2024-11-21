a3932ad1a0a4c2d1d321d116629604f03be64e71
Kevin Dillon <kdillo893@gmail.com>
2024-11-21 15:40:44 -0600

    WIP mm following slides

--- cs351_sp2019/mps/05_mm/mm.c
+++ cs351_sp2019/mps/05_mm/mm.c
@@ -1,6 +1,7 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
  */
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,7 +17,7 @@
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
+/* size of data? */
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 /* 
@@ -27,20 +28,58 @@ int mm_init(void)
   return 0;
 }
 
+void* find_fit(size_t size) {
+  size_t *header = mem_heap_lo();
+  while (header < (size_t*) mem_heap_hi()) {
+    // if this space isn't allocated and size is smaller or equal to open space
+    if (!(*header & 1) && *header >= size) {
+      return header;
+    }
+    //move header forward the size of this space
+    header = (size_t*) header + (*header & ~1L);
+  }
+
+  return NULL;
+}
+
+void* coallesce(size_t *bp) {
+
+  size_t *next, *prev;
+  int next_alloc, prev_alloc;
+
+  //deal with edge cases:
+  if ((size_t *) mem_heap_lo() < bp) {
+    prev = bp - (*(size_t *)bp - SIZE_T_SIZE & ~1L);
+  } else {
+    prev_alloc = 1;
+  }
+
+  if ((size_t *) mem_heap_hi() > bp) {
+    next = bp - (*(size_t *)bp - SIZE_T_SIZE & ~1L);
+  } else {
+    next_alloc = 1;
+  }
+
+  return next;
+}
+
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
-void *mm_malloc(size_t size)
+void* mm_malloc(size_t size)
 {
-  int newsize = ALIGN(size + SIZE_T_SIZE);
-  void *p = mem_sbrk(newsize);
-  if ((long)p == -1)
-    return NULL;
-  else {
-    *(size_t *)p = size;
-    return (void *)((char *)p + SIZE_T_SIZE);
+  int blksize = ALIGN(size + SIZE_T_SIZE);
+  size_t *header = find_fit(blksize);
+  if (header) {
+    //split block if possible
+    *(size_t*)((char*)header + blksize) = *header - blksize;
+  } else {
+    header = mem_sbrk(blksize);
   }
+
+  *header = blksize | 1;
+  return (char*) header + SIZE_T_SIZE;
 }
 
 /*
@@ -48,24 +87,26 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+  size_t *header = (size_t*) ptr - SIZE_T_SIZE;
+  *header = *header & ~1L;
 }
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
-void *mm_realloc(void *ptr, size_t size)
+void* mm_realloc(void *ptr, size_t size)
 {
-  void *oldptr = ptr;
+  size_t *header = (size_t *)((char *)ptr - SIZE_T_SIZE);
+  size_t oldsize = *header & ~1L,
+         newsize = ALIGN(size + SIZE_T_SIZE);
   void *newptr;
-  size_t copySize;
-    
-  newptr = mm_malloc(size);
-  if (newptr == NULL)
-    return NULL;
-  copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-  if (size < copySize)
-    copySize = size;
-  memcpy(newptr, oldptr, copySize);
-  mm_free(oldptr);
-  return newptr;
+
+  if (newsize <= oldsize) {
+    return ptr;
+  } else {
+    newptr = mm_malloc(size);
+    memcpy(newptr, ptr, oldsize - SIZE_T_SIZE);
+    mm_free(ptr);
+    return newptr;
+  }
 }
